e69e496b806741300a83d39cf236f0f40319244f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:18 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "cdn_frontdoor_endpoints" {
-  description = "All cdn_frontdoor_endpoint resources"
-  value       = azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints
-}
 output "cdn_frontdoor_endpoints_cdn_frontdoor_profile_id" {
-  description = "List of cdn_frontdoor_profile_id values across all cdn_frontdoor_endpoints"
-  value       = [for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : v.cdn_frontdoor_profile_id]
+  description = "Map of cdn_frontdoor_profile_id values across all cdn_frontdoor_endpoints, keyed the same as var.cdn_frontdoor_endpoints"
+  value       = { for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : k => v.cdn_frontdoor_profile_id }
 }
 output "cdn_frontdoor_endpoints_enabled" {
-  description = "List of enabled values across all cdn_frontdoor_endpoints"
-  value       = [for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : v.enabled]
+  description = "Map of enabled values across all cdn_frontdoor_endpoints, keyed the same as var.cdn_frontdoor_endpoints"
+  value       = { for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : k => v.enabled }
 }
 output "cdn_frontdoor_endpoints_host_name" {
-  description = "List of host_name values across all cdn_frontdoor_endpoints"
-  value       = [for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : v.host_name]
+  description = "Map of host_name values across all cdn_frontdoor_endpoints, keyed the same as var.cdn_frontdoor_endpoints"
+  value       = { for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : k => v.host_name }
 }
 output "cdn_frontdoor_endpoints_name" {
-  description = "List of name values across all cdn_frontdoor_endpoints"
-  value       = [for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : v.name]
+  description = "Map of name values across all cdn_frontdoor_endpoints, keyed the same as var.cdn_frontdoor_endpoints"
+  value       = { for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : k => v.name }
 }
 output "cdn_frontdoor_endpoints_tags" {
-  description = "List of tags values across all cdn_frontdoor_endpoints"
-  value       = [for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : v.tags]
+  description = "Map of tags values across all cdn_frontdoor_endpoints, keyed the same as var.cdn_frontdoor_endpoints"
+  value       = { for k, v in azurerm_cdn_frontdoor_endpoint.cdn_frontdoor_endpoints : k => v.tags }
 }
 
